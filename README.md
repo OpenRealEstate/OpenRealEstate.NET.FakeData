c37ff2b7a881868dc7040581763e048ba55ac97b
Pure Krome <github@world-domination.com.au>
2018-01-09 11:55:57 +1100

    :space_invader: Number 5 is alive!

---
+++ README.md
@@ -0,0 +1,21 @@
+
+
+# OpenRealEstate.NET FakeData
+
+This is a collection of helper methods that enable you to quickly create various fake listings for the [OpenRealEstate](https://https://github.com/OpenRealEstate/OpenRealEstate) domain models. Its intended to help speed up testing scenario's in your test projects by reducing the time to manually write code.
+
+[![Build status](https://ci.appveyor.com/api/projects/status/ugsqanld3hyofqwe/branch/master?svg=true)](https://ci.appveyor.com/project/PureKrome/openrealestate-net-fakedata) [![NuGet](https://img.shields.io/nuget/v/OpenRealEstate.NET.FakeData.svg)](https://www.nuget.org/packages/OpenRealEstate.NET.FakeData) [![NuGet](https://img.shields.io/nuget/dt/OpenRealEstate.NET.FakeData.svg)](https://www.nuget.org/packages/OpenRealEstate.NET.FakeData) [![MyGet Pre Release](https://img.shields.io/myget/openrealestate-net/vpre/OpenRealEstate.NET.FakeData.svg)]()
+
+---
+
+## Contributing
+
+Discussions and pull requests are encouraged :) Please ask all general questions in this repo or pick a specialized repo for specific, targetted issues. We also have a [contributing](https://github.com/OpenRealEstate/OpenRealEstate/blob/master/CONTRIBUTING.md) document which goes into detail about how to do this.
+
+## Code of Conduct
+Yep, we also have a [code of conduct](https://github.com/OpenRealEstate/OpenRealEstate/blob/master/CODE_OF_CONDUCT.md) which applies to all repositories in the OpenRealEstate organisation.
+
+## Feedback
+Yep, refer to the [contributing page](https://github.com/OpenRealEstate/OpenRealEstate/blob/master/CONTRIBUTING.md) about how best to give feedback - either good or needs-improvement :)
+
+---
---
+++ src/OpenRealEstate.FakeData/FakeAgent.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using FizzWare.NBuilder;
+using FizzWare.NBuilder.Generators;
+using OpenRealEstate.NET.Core;
+
+namespace OpenRealEstate.NET.FakeData
+{
+    public class FakeAgent
+    {
+        public static Agent CreateAFakeAgent()
+        {
+            var communications = new List<Communication>
+            {
+                FakeCommunication.CreateAFakeCommunication()
+            };
+
+            if (GetRandom.Int(1, 5) == 1)
+            {
+                communications.Add(FakeCommunication.CreateAFakeCommunication(CommunicationType.Email));
+            }
+
+            return Builder<Agent>.CreateNew()
+                                 .With(x => x.Name, $"{GetRandom.FirstName()} {GetRandom.LastName()}")
+                                 .With(x => x.Communications, communications)
+                                 .Build();
+        }
+
+        public static IEnumerable<Agent> CreateFakeAgents(int numberOfAgents = 1)
+        {
+            if (numberOfAgents <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(numberOfAgents));
+            }
+
+            var agents = new List<Agent>
+            {
+                CreateAFixedAgent()
+            };
+
+            // Start at 1 because the first listing should be the hard-coded one.
+            for (var i = 1; i < numberOfAgents; i++)
+            {
+                agents.Add(CreateAFakeAgent());
+            }
+
+            return agents;
+        }
+
+        public static Agent CreateAFixedAgent(string name = "Mr. John Doe",
+                                              string email = "jdoe@somedomain.com.au",
+                                              string mobilePhone = "0418 123 456",
+                                              string workPhone = "05 1234 5678",
+                                              int order = 1)
+        {
+            var emailCommunocation = new Communication
+            {
+                CommunicationType = CommunicationType.Email,
+                Details = email
+            };
+
+            var mobileCommunication = new Communication
+            {
+                CommunicationType = CommunicationType.Mobile,
+                Details = mobilePhone
+            };
+
+            var workCommunication = new Communication
+            {
+                CommunicationType = CommunicationType.Landline,
+                Details = workPhone
+            };
+
+            return new Agent
+            {
+                Name = name,
+                Order = order,
+                Communications = new List<Communication>
+                {
+                    emailCommunocation,
+                    mobileCommunication,
+                    workCommunication
+                }
+            };
+        }
+    }
+}

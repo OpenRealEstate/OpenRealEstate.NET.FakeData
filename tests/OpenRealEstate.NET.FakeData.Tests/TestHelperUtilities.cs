d6abd9faaea1f5d1a6e1720bea5ac40c9c551834
Pure Krome <github@world-domination.com.au>
2018-01-10 01:30:38 +1100

    Don't forget to add some tests! - [Skip CI]

---
+++ tests/OpenRealEstate.NET.FakeData.Tests/TestHelperUtilities.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using OpenRealEstate.NET.Core;
+using OpenRealEstate.NET.Core.Land;
+using OpenRealEstate.NET.Core.Rental;
+using OpenRealEstate.NET.Core.Residential;
+using OpenRealEstate.NET.Core.Rural;
+
+namespace OpenRealEstate.NET.FakeData.Tests
+{
+    public abstract class TestHelperUtilities
+    {
+        protected IList<Listing> CreateListings(Type listingType,
+                                                int numberOfFakeListings)
+        {
+            if (listingType == typeof(ResidentialListing))
+            {
+                return FakeListings.CreateFakeListings<ResidentialListing>(numberOfFakeListings)
+                                   .Cast<Listing>()
+                                   .ToList();
+            }
+
+            if (listingType == typeof(RentalListing))
+            {
+                return FakeListings.CreateFakeListings<RentalListing>(numberOfFakeListings)
+                                   .Cast<Listing>()
+                                   .ToList();
+            }
+
+            if (listingType == typeof(LandListing))
+            {
+                return FakeListings.CreateFakeListings<LandListing>(numberOfFakeListings)
+                                   .Cast<Listing>()
+                                   .ToList();
+            }
+
+            if (listingType == typeof(RuralListing))
+            {
+                return FakeListings.CreateFakeListings<RuralListing>(numberOfFakeListings)
+                                   .Cast<Listing>()
+                                   .ToList();
+            }
+
+            throw new Exception($"Failed to assert the suggested type: '{listingType}'.");
+        }
+    }
+}
